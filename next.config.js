7035ae2d3d08c098dcabc0c388ce04498810b2aa
team528iacg-eng <team.528.iacg@gmail.com>
2026-04-07 13:20:38 +0530

    fix: empty next.config.js to match working build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
+module.exports = {};
