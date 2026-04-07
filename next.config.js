d654f837d9d5646f185cfafd48341dcf7aae5b54
team528iacg-eng <team.528.iacg@gmail.com>
2026-04-07 12:44:06 +0530

    fix: simplify next.config.js - remove webpack externals

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Allow production builds even if there are type errors
-    // Type checking is still done in CI/editor - this just unblocks deployment
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['bcryptjs', 'socket.io'],
-  },
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), 'bufferutil', 'utf-8-validate'];
-    return config;
   },
 };
 
